d852a0d5edef04342f12244010d4df687ff10cd5
Jonathon Krouth <jon@krouth.net>
2021-02-07 11:53:28 -0600

    Added Orders Page

---
+++ src/orders.php
@@ -0,0 +1,22 @@
+<?php include 'header.php'; ?>
+
+<div class="col-md-2"></div>
+<div class="col-md-8">
+	<p>
+		If you are interested in purchasing some of my pieces and are in the Duluth area, they can be found at:
+	</p>
+	<p>
+		<div>
+			<a href="https://www.twoloons.mn">2 Loons Gallery and Boutique</a>
+		</div>
+
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2732.8851011570496!2d-92.12402929999999!3d46.7671612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52ae4d787b7323a7%3A0x990d61bcecc4e8f9!2sTwo%20Loons%20Gallery%20and%20Boutique!5e0!3m2!1sen!2sus!4v1612126407495!5m2!1sen!2sus" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+	</p>
+	<p>
+		Alternatively, if you are interested in ordering a piece online or would simply like to inquire about what is available, please reach out
+		via email to "sheilasmarvelousstuff@gmail.com".
+	</p>
+</div>
+<div class="col-md-2"></div>
+
+<?php include 'footer.php'; ?>
